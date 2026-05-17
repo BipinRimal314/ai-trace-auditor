160e1d496062e2d8d5ea57c94963553a48f84327
BipinRimal314 <BipinRimal314@gmail.com>
2026-05-17 15:34:55 +0545

    feat(repo): add report combiner + finalize repo package public API

--- src/ai_trace_auditor/repo/__init__.py
+++ src/ai_trace_auditor/repo/__init__.py
@@ -1,5 +1,6 @@
 """Repository ingestion: fetch + scan + combine."""
 
+from ai_trace_auditor.repo.doc_scanner import scan_docs
 from ai_trace_auditor.repo.errors import (
     InvalidRepoURL,
     PrivateRepo,
@@ -8,12 +9,32 @@ from ai_trace_auditor.repo.errors import (
     RepoNotFound,
     RepoTooLarge,
 )
+from ai_trace_auditor.repo.fetcher import clone_repo, parse_github_url
+from ai_trace_auditor.repo.manifest_loader import load_manifest
+from ai_trace_auditor.repo.models import (
+    DocCheck,
+    DocCheckResult,
+    RepoAuditReport,
+    TraceArtifact,
+)
+from ai_trace_auditor.repo.report import combine_repo_report
+from ai_trace_auditor.repo.trace_finder import find_trace_artifacts
 
 __all__ = [
+    "DocCheck",
+    "DocCheckResult",
     "InvalidRepoURL",
     "PrivateRepo",
+    "RepoAuditReport",
     "RepoError",
     "RepoFetchTimeout",
     "RepoNotFound",
     "RepoTooLarge",
+    "TraceArtifact",
+    "clone_repo",
+    "combine_repo_report",
+    "find_trace_artifacts",
+    "load_manifest",
+    "parse_github_url",
+    "scan_docs",
 ]
